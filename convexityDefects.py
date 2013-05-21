5d06c02a82ac797fceded3749bd7123b15f5b19c
chengdujin <chengdujin@gmail.com>
2013-05-21 17:05:57 +0800

    replace myhand with a jpeg a simple version

--- convexityDefects.py
+++ convexityDefects.py
@@ -2,7 +2,7 @@ from SimpleCV import *
 
 #cam = JpegStreamCamera('http://192.168.1.100:8080/videofeed')
 cam = JpegStreamCamera('http://172.17.200.202:8080/videofeed')
-
+'''
 while True:
     img = cam.getImage().threshold(128)
     blobs = img.findBlobs()
@@ -13,14 +13,29 @@ while True:
     #img.dl().polygon(blob.mConvexHull, color=Color.RED, width=3)
     img.show()
 '''
-
+'''
 img = Image('lenna')
-img = img.threshold(128)
 blobs = img.findBlobs()
 blob = blobs[-1]
-#lines, farpoints = blob.getConvexityDefects()
-#lines.draw()
-#farpoints.draw(color=Color.RED, width=-1)
-img.dl().polygon(blob.mConvexHull, color=Color.RED, width=3)
+lines, farpoints = blob.getConvexityDefects()
+lines.draw()
+farpoints.draw(color=Color.RED, width=-1)
 img.show()
 '''
+
+
+while True:
+    img = Image('/home/jinyuan/Downloads/handmagician/my_hand_mod.jpg')
+    #img = cam.getImage()
+    #img = Image('lenna')
+    img = img.threshold(200)
+    blobs = img.findBlobs()
+    blob = blobs[-1]
+    if blob.area() < 80000:
+        print 'oh ...', blob.area()
+        lines, farpoints = blob.getConvexityDefects()
+        lines.draw()
+        farpoints.draw(color=Color.RED, width=3)
+    img.dl().polygon(blob.mConvexHull, color=Color.RED, width=3)
+    print 'no ... ', blob.area()
+    img.show() 
